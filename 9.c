4793868140c74f3aa5565ff98a489b413207490b
vkitise <151126627+vkitise@users.noreply.github.com>
2025-05-11 15:35:49 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,43 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-void selectionSort(int arr[], int n){
-int i,j,minIndex,temp;
+void selectionSort(int a[],int n){
+int i,j,min,t;
 for(i=0;i<n-1;i++){
-minIndex=i;
-for(j=i+1;j<n;j++){
-if(arr[j]<arr[minIndex]){
-minIndex=j;
+min=i;
+for(j=i+1;j<n;j++)
+if(a[j]<a[min])min=j;
+t=a[min];a[min]=a[i];a[i]=t;
 }
 }
-temp=arr[minIndex];
-arr[minIndex]=arr[i];
-arr[i]=temp;
-}
-}
-int generateRandomNumber(){
-return rand()%1000;
-}
+int getRandom(){return rand()%1000;}
 int main(){
-int n=6000;
-int* arr=(int*)malloc(n*sizeof(int));
+int n=6000,*a=malloc(n*sizeof(int));
 srand(time(NULL));
 printf("Random numbers for n=%d:\n",n);
-for(int i=0;i<n;i++){
-arr[i]=generateRandomNumber();
-printf("%d ",arr[i]);
-}
-printf("\n");
-clock_t start=clock();
-selectionSort(arr,n);
-clock_t end=clock();
-double time_taken=((double)(end-start))/CLOCKS_PER_SEC;
-printf("\nTime taken to sort for n=%d: %lf seconds\n\n",n,time_taken);
+for(int i=0;i<n;i++){a[i]=getRandom();printf("%d ",a[i]);}
+clock_t s=clock();
+selectionSort(a,n);
+clock_t e=clock();
+double t=(double)(e-s)/CLOCKS_PER_SEC;
+printf("\n\nTime taken to sort for n=%d: %lf seconds\n\n",n,t);
 printf("Sorted numbers for n=%d:\n",n);
-for(int i=0;i<n;i++){
-printf("%d",arr[i]);
-}
+for(int i=0;i<n;i++)printf("%d",a[i]);
 printf("\n\n");
-free(arr);
+free(a);
 return 0;
 }
