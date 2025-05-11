af7a4a7581518ef16e0d6b10a900107f3089042d
vkitise <151126627+vkitise@users.noreply.github.com>
2025-05-11 15:37:30 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,70 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-void merge(int arr[],int l,int m,int r){
-int i,j,k;
-int n1=m-l+1;
-int n2=r-m;
-int L[n1],R[n2];
-for(i=0;i<n1;i++)
-L[i]=arr[l+i];
-for(j=0;j<n2;j++)
-R[j]=arr[m+1+j];
-i=0;
-j=0;
-k=l;
-while(i<n1&&j<n2){
-if(L[i]<=R[j]){
-arr[k]=L[i];
-i++;
-}else{
-arr[k]=R[j];
-j++;
+void merge(int a[],int l,int m,int r){
+int n1=m-l+1,n2=r-m,L[n1],R[n2];
+for(int i=0;i<n1;i++)L[i]=a[l+i];
+for(int j=0;j<n2;j++)R[j]=a[m+1+j];
+int i=0,j=0,k=l;
+while(i<n1&&j<n2)a[k++]=(L[i]<=R[j])?L[i++]:R[j++];
+while(i<n1)a[k++]=L[i++];
+while(j<n2)a[k++]=R[j++];
 }
-k++;
-}
-while(i<n1){
-arr[k]=L[i];
-i++;
-k++;
-}
-while(j<n2){
-arr[k]=R[j];
-j++;
-k++;
-}
-}
-void mergeSort(int arr[],int l,int r){
+void mergeSort(int a[],int l,int r){
 if(l<r){
-int m=l+(r-l)/2;
-mergeSort(arr,l,m);
-mergeSort(arr,m+1,r);
-merge(arr,l,m,r);
+int m=(l+r)/2;
+mergeSort(a,l,m);
+mergeSort(a,m+1,r);
+merge(a,l,m,r);
 }
 }
-int generateRandomNumber(){
-return rand()%1000;
-}
+int getRandom(){return rand()%1000;}
 int main(){
-int n=6000;
-int* arr=(int*)malloc(n*sizeof(int));
+int n=6000,*a=malloc(n*sizeof(int));
 srand(time(NULL));
 printf("Random numbers for n=%d:\n",n);
-for(int i=0;i<n;i++){
-arr[i]=generateRandomNumber();
-printf("%d ",arr[i]);
-}
-printf("\n");
+for(int i=0;i<n;i++){a[i]=getRandom();printf("%d ",a[i]);}
 clock_t start=clock();
-mergeSort(arr,0,n-1);
+mergeSort(a,0,n-1);
 clock_t end=clock();
-double time_taken=((double)(end-start))/CLOCKS_PER_SEC;
-printf("\nTimetaken tosort forn=%d: %lfseconds\n\n",n,time_taken);
+double time=(double)(end-start)/CLOCKS_PER_SEC;
+printf("\n\nTimetaken tosort forn=%d: %lfseconds\n\n",n,time);
 printf("Sorted numbers for n=%d:\n",n);
-for(int i=0;i<n;i++){
-printf("%d",arr[i]);
-}
+for(int i=0;i<n;i++)printf("%d",a[i]);
 printf("\n\n");
-free(arr);
+free(a);
 return 0;
 }
